3d5827011e2a3d072055014125fe96ae31ef19aa
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-24 18:39:33 -0400

    recursive lambda example

--- SimpleRecursiveLambda/main.cpp
+++ SimpleRecursiveLambda/main.cpp
@@ -1,14 +1,22 @@
-//
-//  main.cpp
-//  SimpleRecursiveLambda
-//
-//  Created by Matt Ahmad on 2022-10-24.
-//
-
+#include <functional>
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+    int anyNumber = 123456789;
+    
+    function<void()> reverseByRecursiveLambda;
+    
+    reverseByRecursiveLambda = [&]() {
+        if (anyNumber == 0)
+            return;
+        
+        cout << anyNumber % 10 << " ";
+        anyNumber /= 10;
+        reverseByRecursiveLambda();
+    };
+    
+    reverseByRecursiveLambda();
 }
